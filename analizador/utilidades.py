6f2054734fc3b03ca71c312a644f281519cfb524
Your Name <ibg.indira@gmail.com>
2025-10-28 08:37:46 +0000

    add modularity

---
+++ analizador/utilidades.py
@@ -0,0 +1,13 @@
+def contar_letras(texto):
+    # print(len(texto))
+    return(len(texto))
+
+def contar_palabras(texto):
+    # print(len(texto.split()))
+    return(len(texto.split()))
+
+def a_mayusculas(texto):
+    return texto.upper()
+
+contar_letras("sol")
+contar_palabras("sol solet")
---
+++ textos.py
@@ -0,0 +1,7 @@
+import Programming_fundamentals.analizador.utilidades as utilidades
+
+mi_frase= "La modularidad es la clave del software profesional"
+
+print(utilidades.contar_letras(mi_frase))
+print(utilidades.contar_palabras(mi_frase))
+print(utilidades.a_mayusculas(mi_frase))
